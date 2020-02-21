bfec2e2d5aa824d22bbc48b62aa3830de68ccb1b
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2020-02-20 22:32:27 -0600

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,24 +1,24 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
-    .then(ref=>console.log('Exito',reg))
-    .catch(err=>console.warn('Error'))
+    navigator.serviceWorker.register('./sw.js');
+    .then(ref=>console.log('Exito',reg));
+    .catch(err=>console.warn('Error'));
 }
-var cont = 1
-var whi = true
+var cont = 1;
+var whi = true;
 
 
 function Next() {
 
-    cont++
-    whi = true
+    cont++;
+    whi = true;
     if (cont > 5) {
-        cont = 1
+        cont = 1;
     }
-    var image = document.getElementById("imagen")
-    image.src = cont + ".png"
+    var image = document.getElementById("imagen");
+    image.src = cont + ".png";
 
-    clearInterval(int)
-    int=setInterval(It,3000)
+    clearInterval(int);
+    int=setInterval(It,3000);
 
 
 }
@@ -26,132 +26,121 @@ function Next() {
 
 function Prev() {
 
-    cont--
-    whi = false
+    cont--;
+    whi = false;
     if (cont < 1) {
-        cont = 5
+        cont = 5;
     }
 
-    var image = document.getElementById("imagen")
-    image.src = cont + ".png"
+    var image = document.getElementById("imagen");
+    image.src = cont + ".png";
 
-    clearInterval(int)
-    int=setInterval(It,3000)
+    clearInterval(int);
+    int=setInterval(It,3000);
 }
 
 function It() {
     if (whi == true) {
-        cont++
+        cont++;
         if (cont > 5) {
-            cont = 1
+            cont = 1;
         }
     } else {
-        cont--
+        cont--;
 
         if (cont < 1) {
-            cont = 5
+            cont = 5;
         }
     }
-
-    var image = document.getElementById("imagen")
-    image.src = cont + ".png"
+    
+    var image = document.getElementById("imagen");
+    image.src = cont + ".png";
 
 }
 
 
-var int = setInterval(It, 3000)
+var int = setInterval(It, 3000);
 
 
 //Formulario
-let array=[]
-let array2=[]
-if(localStorage.getItem("keys")!=null){
-    let a=localStorage.getItem("keys")
-    array=JSON.parse(a)
-    let b=localStorage.getItem("array")
-    array2=JSON.parse(b)
-}else{
-    let array=[]
-    let array2=[]
-}
 
+let array=[];
+    
+$(document).ready(function () {
+    
 
-function Add(){
-
-   let id=document.getElementById("id").value
-    let name=document.getElementById("name").value
-    let age=document.getElementById("age").value
-    let city=document.getElementById("city").value
-    let tel=document.getElementById("tel").value
-    let pay=document.getElementById("pay").value
-
-let arr={
-    id:id,
-    nombre:name,
-    edad:age,
-    ciudad:city,
-    tele:tel,
-    pago:pay
-}
-
-
-//let arr=[]
-//arr=[id,name,age,city,tel,pay]
-array.push(arr)
-array2.push(arr)
-localStorage.setItem("keys",JSON.stringify(array))
-
-localStorage.setItem("array",JSON.stringify(array2))
-Charge()
-}
-
-function Charge(){
-    var table = document.getElementById("tabla");
-    table.innerHTML=''
-    var back=localStorage.getItem("keys")
-    var obj=JSON.parse(back)
-    for (var element in obj){
-
-    var row = table.insertRow(0 );
-
-// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3= row.insertCell(2);
-var cell4= row.insertCell(3);
-var cell5= row.insertCell(4);
-var cell6 = row.insertCell(5);
-
-
-
-// Add some text to the new cells:
-cell1.innerHTML = obj[element].nombre;
-cell2.innerHTML = obj[element].edad;
-cell3.innerHTML = obj[element].ciudad;
-cell4.innerHTML = obj[element].tele;
-cell5.innerHTML = obj[element].pago;
-cell6.innerHTML=obj[element].id
+    $("#save").click(function () { 
+        let array2=[];
+    if(localStorage.getItem("keys")!=null){
+        let a=localStorage.getItem("keys");
+        array=JSON.parse(a);
+        let b=localStorage.getItem("array")
+        array2=JSON.parse(b);
+    }else{
+        let array=[];
+        let array2=[];
     }
-       //    var obj=JSON.parse(back)
-// Create an empty <tr> element and add it to the 1st position of the table:
+        var aidi=$("#id").val();
+        var n=$('#name').val();
+        var a=$("#age").val();
+        var ci=$("#city").val();
+        var sco=$("#sco").val();
+        var arr={
+            id:aidi,
+            name: n,
+            age: a,
+            city: ci,
+            school:sco
+        }
+        array.push(arr);
+array2.push(arr);
+localStorage.setItem("keys",JSON.stringify(array));
+
+localStorage.setItem("array",JSON.stringify(array2));
+ 
+        charge();
+    });
+    
+    $('#save').click(function (e) { 
+        charge() ;       
+    });
+   
+    $('#erase').click(function (e) { 
+        e.preventDefault();
+        remove();
+    });
+});
+
+function charge(){
+    var table = document.getElementById("tabla");
+    table.innerHTML='';
+    var back=localStorage.getItem("keys");
+    var obj=JSON.parse(back);
+    
+    for (var element in obj){
+        $('tbody').append("<tr><td>"+obj[element].id+"</td><td>"+obj[element].name+"</td><td>"+obj[element].city+"</td><td>"+obj[element].age+"</td><td>"+obj[element].school+"</td></tr>");
+
+    }
     
     }
-
-function remove(){
-    var num=document.getElementById("borrar").value
-    var back=localStorage.getItem("keys")
-    var obj=localStorage.getItem("array")
-    var array2=JSON.parse(obj)
-    for(var element in array2){
-        if(array2[element].id==num){
-            console.log("entro")
-            array2.splice(element,1)
-            localStorage.setItem("keys",JSON.stringify(array2) )
+    function remove(){
+        var num=document.getElementById("borrar").value;
+        var back=localStorage.getItem("keys");
+        var obj=localStorage.getItem("array");
+        var array2=JSON.parse(obj);
+        for(var element in array2){
+            if(array2[element].id==num){
+                array2.splice(element,1)
+                localStorage.setItem("keys",JSON.stringify(array2) );
+            }
         }
+        localStorage.setItem("array",JSON.stringify(array2) );
+        charge();
     }
-    localStorage.setItem("array",JSON.stringify(array2) )
-    Charge()
-}
+
+
+
+
 
 
 
