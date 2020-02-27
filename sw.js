f430eb843bae9583e6d999ce8b119f2989ef2090
Cesar-Javier-Ramirez-Manzo <50338995+Cesar-Javier-Ramirez-Manzo@users.noreply.github.com>
2020-02-27 16:56:21 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ urlToCache=[
     './square.png',
     './star.png',
     './jquery-3.4.1.min.js',
-    'https://unpkg.com/purecss@1.0.1/build/pure-min.css" integrity="sha384-oAOxQR6DkCoMliIh8yFnu25d7Eq/PHS21PClpwjOTeU2jRSq11vu66rf90/cZr47"'
+    'https://unpkg.com/purecss@1.0.1/build/pure-min.css'
 
 
 ]
